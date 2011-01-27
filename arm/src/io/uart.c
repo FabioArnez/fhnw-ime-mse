7b44a066df4e17fa654331a99e3c5a708c5c420d
hans.buchmann <hans.buchmann@02dc74c5-0b13-4912-8574-b021f93d0eda>
2011-01-27 20:24:10 +0000

    uart
    git-svn-id: http://websvn.fhnw.ch/master/RTSystem/buchmann@262 02dc74c5-0b13-4912-8574-b021f93d0eda

---
+++ arm/src/io/uart.c
@@ -0,0 +1,27 @@
+/*----------------------
+ uart
+ (c) H.Buchmann FHNW 2011
+ $Id$
+ see [IntegratorCM] [UART]
+ -----------------------*/
+
+volatile struct 
+{
+ unsigned DR;                         /*Data register */ 
+ unsigned SR;                       /* Receive status */
+ unsigned res[4];                         /* reserved */
+ unsigned FR;                                 /* Flag */  
+}* UART = 0x16000000;
+
+void uart_out(char ch)
+{
+ while(UART->FR&(1<<3)){} /* wait until busys cleared */
+ UART->DR=ch;
+}
+
+char uart_in()
+{
+ while(UART->FR&(1<<4)){} /* wait until rx ready */
+ return UART->DR;
+}
+
---
+++ arm/src/io/uart.h
@@ -0,0 +1,8 @@
+#pragma once
+/*----------------------
+ uart
+ (c) H.Buchmann FHNW 2011
+ $Id$
+ -----------------------*/
+extern void uart_out(char ch);
+extern char uart_in();
