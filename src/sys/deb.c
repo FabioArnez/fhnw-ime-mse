90351b03daa8d47d0f633fd6da38903a24fa94b5
hans.buchmann <hans.buchmann@02dc74c5-0b13-4912-8574-b021f93d0eda>
2011-01-27 19:36:00 +0000

    arm/intel
    git-svn-id: http://websvn.fhnw.ch/master/RTSystem/buchmann@257 02dc74c5-0b13-4912-8574-b021f93d0eda

---
+++ src/sys/deb.c
@@ -0,0 +1,9 @@
+/*--------------------
+ deb(ug)
+ (c) H.Buchmann FHNW 2009
+ $Id: deb.c 200 2010-03-13 17:20:44Z hans.buchmann $
+----------------------*/
+#include "sys/deb.h"
+#include "sys/sys.h"
+#error "implement in particular architecture"
+
