32c2c46bb515dc1cba4eb055bb68896527c6693d
Hans Buchmann <hans.buchmann@fhnw.ch>
2011-02-02 14:59:40 +0100

    some exercises, in 'sos' 'john-von-neumann' removed

---
+++ src/hello-world.c
@@ -0,0 +1,10 @@
+/*---------------------
+ hello-world
+ (c) H.Buchmann FHNW 2011
+ $Id$
+-----------------------*/
+#include <stdio.h>
+int main()
+{
+ printf("Hello World date='%s' file='%s'\n",__DATE__,__FILE__);
+}
