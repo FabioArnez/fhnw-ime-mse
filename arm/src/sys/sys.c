c4d37c6e2b6161eb032941e65f20d93a2784377d
Hans Buchmann <hans.buchmann@fhnw.ch>
2011-03-04 16:51:32 +0100

    big-bang exception table

--- arm/src/sys/sys.c
+++ arm/src/sys/sys.c
@@ -3,13 +3,14 @@
  (c) H.Buchmann FHNW 2011
  $Id$
  see [1] literature/ARM-Architecture-Reference-Manual.pdf
+     [2] http://gcc.gnu.org/onlinedocs/gcc-4.5.2/gcc/
 ----------------------*/
 #include "sys/sys.h"
 void sys_undef()
 {
- asm volatile
+ asm volatile /* see [2] Extended-Asm.html#Extended-Asm */
  (
-  ".word 0xe7f000f0"  /* see [1] A3.16.5 p 147 */
+  ".word 0xe7f000f0"           /* see [1] A3.16.5 p 147 */
   :
   :
  );
