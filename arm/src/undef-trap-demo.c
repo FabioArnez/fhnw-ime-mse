09279a1fc606eb7dd401703a96de9d0e5417346f
Hans Buchmann <hans.buchmann@fhnw.ch>
2011-03-04 18:07:45 +0100

    calling onMyUndef

--- arm/src/undef-trap-demo.c
+++ arm/src/undef-trap-demo.c
@@ -5,12 +5,32 @@
 ----------------------*/
 #include <stdio.h>
 #include "sys/sys.h" /* for triggering undef */
+#include "sys/deb.h"
+
+void onMyUndef()
+{
+ deb_signal1();
+}
+
+typedef void (*OnUndef)();
+
+extern OnUndef onUndef; /* see big-bang.S*/	
+
+void registerUndef(OnUndef oud)
+{
+ onUndef=oud;
+}
+
 int main()
 {
  unsigned v=0;
+ registerUndef(onMyUndef);
+ printf("%p\n",*onUndef);
+#if 1 
  while(1)
  {
   printf("trap-demo %d\n",v++);
   sys_undef();
- } 
+ }
+#endif
 }  
