611e6cd548e722a7c5858c3b8a51b5305dfe4189
Hans Buchmann <hans.buchmann@fhnw.ch>
2011-03-05 20:34:52 +0100

    IRQ

--- arm/src/timer-trap-demo.c
+++ arm/src/timer-trap-demo.c
@@ -5,6 +5,15 @@
   [1] literature/DUI0159B_integratorcp_1_0_ug.pdf
 ------------------------*/
 #include "stdio.h"
+#include "sys/sys.h"
+                                       /* same struct for FIQ struct */
+volatile struct                                     /* see [3] 3.6.1 */
+{
+ const unsigned STAT;
+ const unsigned RSTAT;
+ unsigned ENSET;
+ unsigned ENCLR;
+}* PRIMIRQ = 0x14000000;
 
 volatile struct /* [1] 4.9.2 */
 {
@@ -19,15 +28,17 @@ volatile struct /* [1] 4.9.2 */
 
 void main()
 {
- TIMER0->load=0xffff;
+ TIMER0->load=1<<25;
  TIMER0->control=(0<<0)| /* wrapping mode */
-                 (0<<1)| /* 16 bit counter */
+                 (1<<1)| /* 32 bit counter */
 		 (0<<2)| /* prescale 0 */
-		 (0<<5)| /* no interrupt */
+		 (1<<5)| /* IE */
 		 (1<<6)| /* periodic */
 		 (1<<7); /* enable */
+ sys_setCPSR(sys_getCPSR()&~(1<<7)); /* enable IRQ */
+ PRIMIRQ->ENSET=(1<<5);
  while(1)
  {
-  printf("%x\n",TIMER0->value);
+  printf("%x %x\n",TIMER0->value,PRIMIRQ->RSTAT);
  }
 } 
