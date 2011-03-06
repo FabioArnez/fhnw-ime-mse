a7554d75aec36435e566d1c4af2834eb69499f4f
Hans Buchmann <hans.buchmann@fhnw.ch>
2011-03-06 19:42:46 +0100

    selftest

--- intel/src/kbdc-demo.c
+++ intel/src/kbdc-demo.c
@@ -2,23 +2,38 @@
   kbdc-demo keyboard controller demo
   (c) H.Buchmann FHNW 2011
   $Id$
+  [1] http://www.win.tue.nl/~aeb/linux/kbd/scancodes-11.html
 --------------------------*/
 #include "sys/sys.h" /* for sys_inb sys_outb */
 #include "stdio.h"
+
+typedef unsigned char byte;
+
+void out60(byte v)
+{
+ printf("out60\n");
+ while(sys_inb(0x64)&(1<<1)){} /* wait until ready to write */
+ sys_outb(v,0x60);
+}
+
+void out64(byte v)
+{
+ printf("out64\n");
+ while(sys_inb(0x64)&(1<<1)){} /* wait until ready to write */
+ sys_outb(v,0x64);
+}
+
+byte in60()
+{
+ printf("in60\n");
+ while((sys_inb(0x64)&(1<<0))==0){} /* wait until ready to write */
+ return sys_inb(0x60);
+}
+
 void main()
 {
-/* 1c 1d */
- unsigned stat0=sys_inb(0x64);
- printf("%x\n",stat0); /* status */
- 
- while(1)
- {
-  unsigned stat1=sys_inb(0x64);
-  if (stat0!=stat1)
-     {
-      printf("%x\n",stat1); /* status */
-     }
-  stat0=stat1;
- }
+ /* selftest */
+ out64(0xaa);
+ in60());
 }
   
