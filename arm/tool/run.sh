a93e08dc6da0a2903eb357822afbc270607d7bd5
Hans Buchmann <hans.buchmann@fhnw.ch>
2012-01-23 16:40:59 +0100

    first.S for arm realview-eb

--- arm/tool/run.sh
+++ arm/tool/run.sh
@@ -5,6 +5,7 @@
 #accessible from work
 #------------------------
 QEMU=../../../qemu
-${QEMU}/bin/qemu-system-arm -kernel ${1} -nographic
+#${QEMU}/bin/qemu-system-arm -kernel ${1} -nographic
+#${QEMU}/bin/qemu-system-arm -M realview-eb -kernel ${1} -nographic
+qemu-system-arm -M realview-eb -kernel ${1} -nographic
 stty echo
-
