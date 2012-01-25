3da90c221ea447268d7d6c8a1e0f41a8d297d32f
Hans Buchmann <hans.buchmann@fhnw.ch>
2012-01-25 14:23:41 +0100

    hello-world for realview

--- arm/tool/run.sh
+++ arm/tool/run.sh
@@ -4,8 +4,15 @@
 #$Id$
 #accessible from work
 #------------------------
-QEMU=../../../qemu
-#${QEMU}/bin/qemu-system-arm -kernel ${1} -nographic
-#${QEMU}/bin/qemu-system-arm -M realview-eb -kernel ${1} -nographic
+if [ -z ${1} ]
+  then echo "usage ${0} imageFile"
+       exit 1
+fi
+
+if [ ! -f ${1} ]
+   then echo "File ${1} dont exists"
+        exit 1
+fi
+
 qemu-system-arm -M realview-eb -kernel ${1} -nographic
 stty echo
