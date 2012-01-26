6dea4218a8a890ac4fb6912960658e1907d15d0f
Hans Buchmann <hans.buchmann@fhnw.ch>
2012-01-26 15:41:08 +0100

    some todo's

---
+++ tool/Rules.java
@@ -0,0 +1,11 @@
+//-----------------------------
+//Rules 
+// shows simple makefile rules graphically using graphviz
+//(c) H.Buchmann FHNW 2012
+//$Id$
+//-----------------------------
+
+class Rules
+{
+ //TODO 
+}
