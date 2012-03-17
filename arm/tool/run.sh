c1a6e1efc4e211e76146f92becc1fb4fec902eb5
Hans Buchmann <hans.buchmann@fhnw.ch>
2012-03-17 10:51:05 +0100

    with empty vic

--- arm/tool/run.sh
+++ arm/tool/run.sh
@@ -13,9 +13,10 @@ if [ ! -f ${1} ]
    then echo "File ${1} dont exists"
         exit 1
 fi
+MACHINE=versatileab
 QEMU=../../../qemu/bin/
 #QEMU=
-${QEMU}qemu-system-arm -M realview-eb -kernel ${1} -serial stdio -sdl -display sdl
+${QEMU}qemu-system-arm -M ${MACHINE} -kernel ${1} -serial stdio -sdl -display sdl
 #-chardev pipe,id=console,path=console
 #-nographic
 stty echo
