7d84bfb75d288cd5b508810cea4f1c9aa329b476
Hans Buchmann <hans.buchmann@fhnw.ch>
2012-03-25 15:42:38 +0200

    ready for demo

--- arm/src/page-fault-demo.c
+++ arm/src/page-fault-demo.c
@@ -7,15 +7,11 @@
 #include "sys/arm.h"
 static __attribute__((interrupt("ABORT"))) void onData()
 {
- unsigned* pc=arm_get_lr();
- unsigned* mem =arm_get_fault_address();
- printf("onData pc=%p\tmem=%p\n",pc,mem);
 }
 
 int main()
 {
  arm_init(); /* dont forget */
- arm_set_exception(DATA,onData);
  unsigned* p=(unsigned*)(64*(1<<20)); /* no memory there: see mmc.c */
  printf("before assignment\n");
  *p=0;
