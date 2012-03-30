686e416578c8d060e7e17d820ca7fee9b9f63426
Hans Buchmann <hans.buchmann@fhnw.ch>
2012-03-30 18:40:23 +0200

    debug

--- arm/tool/run.sh
+++ arm/tool/run.sh
@@ -13,6 +13,13 @@ if [ ! -f ${1} ]
    then echo "File ${1} dont exists"
         exit 1
 fi
+DEB="-${2}"
+
+if [ ${DEB} = "-DEBUG" ]
+   then DEBUG='-s -S'
+        echo "debug mode"
+fi
+
 IMG=${1}
 IMGFILE=${IMG}.img
 if [ -f ${IMGFILE} ] 
@@ -26,7 +33,7 @@ MACHINE=realview-eb
 #MACHINE=versatileab
 
 QEMU=../../../qemu/bin/
-QEMU=
+#QEMU=
 ${QEMU}qemu-system-arm -M ${MACHINE} -kernel ${IMG} \
 ${DEBUG} \
 -serial stdio -sdl -display sdl
