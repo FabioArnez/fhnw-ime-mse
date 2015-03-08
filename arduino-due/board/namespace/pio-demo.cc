8098409368953b7021234a2660adb60b28a2ffc6
H.Buchmann <hans.buchmann@fhnw.ch>
2015-03-08 18:03:20 +0100

    the exercise

--- arduino-due/board/namespace/pio-demo.cc
+++ arduino-due/board/namespace/pio-demo.cc
@@ -20,20 +20,9 @@ class Demo
 
 Demo Demo::demo;
 
-void Demo::onPin()
-{
- sys::msg<<"pin "<<io::ascii::hex()<<sys::SOC::PIO_B.ISR<<"\n"; //clears interrupt
-}
-
 Demo::Demo()
 {
  sys::msg<<"PIO-Demo\n";
- sys::SOC::clockEnable(sys::SOC::PIOB);
- sys::SOC::PIO_B.set(sys::reg::PIO::IN,INPUT);
- sys::SOC::install(sys::SOC::PIOB,onPin);
- sys::SOC::arm(sys::SOC::PIOB); 
- sys::SOC::PIO_B.IER=INPUT;
- 
  while(true)
  {
   sys::deb::out(sys::deb::get());
